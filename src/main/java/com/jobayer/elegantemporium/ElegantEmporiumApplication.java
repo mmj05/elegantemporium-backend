1a97b3c4421f2996e817cfe4a405d25126886e84
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-02 21:18:14 -0500

    initial commit

---
+++ src/main/java/com/jobayer/elegantemporium/ElegantEmporiumApplication.java
@@ -0,0 +1,13 @@
+package com.jobayer.elegantemporium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegantEmporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElegantEmporiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobayer/elegantemporium/ElegantemporiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobayer.elegantemporium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElegantemporiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
